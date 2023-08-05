ed62cbd3aaf037d9988db615f3f9a8c11047bef5
Amadimm <amadeuszrolecki@gmail.com>
2023-08-05 02:05:14 +0200

    ff

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,52 @@
+import { useDispatch } from 'react-redux';
+import css from './ContactForm.module.css';
+import { addContact } from 'redux/contactsSlice';
+
+export const ContactForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.elements.name.value;
+    const number = form.elements.number.value;
+
+    dispatch(addContact(name, number));
+
+    form.reset();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className={css.form}>
+        <label className={css.label}>
+          Name
+          <input
+            type="text"
+                      name="name"
+                      pattern="^[A-Za-z.'\- ]+$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            className={css.input}
+          />
+        </label>
+
+        <label className={css.label}>
+          Number
+          <input
+            type="tel"
+                      name="number"
+                       pattern="^\+?\d{1,4}?\s?\(?\d{1,4}?\)?\s?\d{1,4}\s?\d{1,4}\s?\d{1,9}$"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            className={css.input}
+          />
+        </label>
+
+        <button type="submit" className={css.btn}>
+          Add contact
+        </button>
+      </form>
+    </div>
+  );
+};
